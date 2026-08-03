49145e85967f0b69e6d104864edefda0de3d3001
Semawit Berhe <saberhe@tudelft.nl>
2026-08-03 14:56:25 +0200

    Fix evaluation pipeline name

---
+++ pipelines/evaluation_pipeline.py
@@ -0,0 +1,4 @@
+from src.model_evaluation.evaluate import evaluate
+
+if __name__ == "__main__":
+    evaluate()
---
+++ pipelines/prediction_pipeline.py
@@ -0,0 +1,4 @@
+from src.model_prediction.predict import predict
+
+if __name__ == "__main__":
+    predict()
