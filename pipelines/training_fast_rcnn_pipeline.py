7258ac319024c04f835f6c2b8ee88d8a06a74973
Semawit Berhe <saberhe@tudelft.nl>
2026-08-05 15:03:07 +0200

    update training code for RCNN

---
+++ pipelines/training_fast_rcnn_pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+
+from src.model_training.train_fast_rcnn import (
+    train_model
+)
+
+
+if __name__ == "__main__":
+
+    train_model()
--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,4 +1,14 @@
 import yaml
-with open("configs/config.yaml","r") as file:
-    config=yaml.safe_load(file)
-    print(config)
+
+import sys
+import os
+sys.path.append(os.getcwd())
+with open("configs/rcnn_params.yaml","r") as file:
+    params=yaml.safe_load(file)
+
+
+    print(params["training"]["batch_size"])
+    print(params["classes"])
+
+
+
